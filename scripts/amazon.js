79d5355095bdab2066340bf53cf98546bfdc1eec
barakamukungu <bethelybaraka@gmail.com>
2025-03-07 16:25:29 +0300

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -17,7 +17,7 @@ function renderProductsGrid() {
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="ratings/${product.rating}.png">
+          <img class="product-rating-stars" src="ratings/rating-${product.rating}.png">
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
